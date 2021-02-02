c40028504e6c5772ec985d71b6a7da5495e647f1
fercho2798 <47802477+fercho2798@users.noreply.github.com>
2021-02-02 15:06:15 -0500

    Add files via upload

---
+++ Calculadora.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Matematica
+{
+    public class Calculadora
+    {
+        public int Sumar(int sumando1, int sumando2)
+        {
+            return sumando1 + sumando2;
+        }
+        public int Dividir(int dividiendo1, int dividiendo2)
+        {
+            return dividiendo1 / dividiendo2;
+        }
+        public int Multiplicar(int multiplicando1, int multiplicando2)
+        {
+            return multiplicando1 * multiplicando2;
+        }
+        public int Resta(int restando1, int restando2)
+        {
+            return restando1 - restando2;
+        }
+        public int Potencia(int potenciando1, int potenciando2)
+        {
+          return potenciando1 ^ potenciando2;
+        }
+        public int PotenciaCuadrada(int potenciando)
+        {
+            return potenciando * potenciando;
+        }
+
+        public double Raiz(double raiz)
+        {
+            return Math.Sqrt(raiz);
+
+        }
+        public double Inversa(double inversando)
+        {
+            return 1 / inversando;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Matematica;
+
+namespace HolaMundoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el primer número: ");
+            var numero1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo número: ");
+            var numero2 = int.Parse(Console.ReadLine());
+            var calculadora = new Calculadora();
+            var resultado = calculadora.Sumar(numero1, numero2);
+            var resultadoDividir = calculadora.Dividir(numero1, numero2);
+            var resultadoMultiplicar = calculadora.Multiplicar(numero1, numero2);
+            var resultadoRestar = calculadora.Resta(numero1, numero2);
+            var resultadoPotenciar = calculadora.Potencia(numero1, numero2);
+            var resultadoPotenciaCuadrada = calculadora.PotenciaCuadrada(numero1);
+            var resultadoRaiz = calculadora.Raiz(numero1);
+            var resultadoInversa = calculadora.Inversa(numero1);
+
+            Console.WriteLine("El resultado de la suma es: " + resultado);
+            Console.WriteLine("El resultado de la divicion es: " + resultadoDividir);
+            Console.WriteLine("El resultado de la multiplicacion es: " + resultadoMultiplicar);
+            Console.WriteLine("El resultado de la resta es: " + resultadoRestar);
+            Console.WriteLine(numero1 + " Elevado a la potencia " + numero2 + " es " + resultadoPotenciar);
+            Console.WriteLine(numero1 + " Elevado al cuadrada es: " + resultadoPotenciaCuadrada);
+            Console.WriteLine("la raiz cuadrada de : " + numero1 + " es: " + resultadoRaiz);
+            Console.WriteLine("La inversa de " + numero1 + " es:  " + resultadoInversa);
+            Console.ReadLine();
+        }
+    }
+}
